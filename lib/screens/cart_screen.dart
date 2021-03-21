1f427a50324a474a2f8c1594a24244b9f6df2ae5
kapiljhajhria <jhajhria44@gmail.com>
2021-03-21 11:29:08 +0530

    extracted total card and displaying it at bottom as well

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -14,48 +14,7 @@ class CartScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Card(
-            margin: EdgeInsets.all(12),
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              //Total Amount and Order Now Button Row
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Total",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Chip(
-                    label: Text(
-                      "\$${cart.totalAmount}",
-                      style: TextStyle(
-                          color: Theme.of(context)
-                              .primaryTextTheme
-                              .headline6!
-                              .color),
-                    ),
-                    backgroundColor: Theme.of(context).primaryColor,
-                  ),
-                  Spacer(),
-                  TextButton(
-                      onPressed: () {
-                        //place the order, move to confirmation or payment screen
-                      },
-                      child: Text(
-                        "ORDER NOW",
-                        style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.bold),
-                      ))
-                ],
-              ),
-              //Summary Cart with all items and option to remove or change their qty
-            ),
-          ),
+          buildTotalCard(cart, context),
           Expanded(
             child: ListView.builder(
                 itemCount: cart.itemCount,
@@ -64,9 +23,55 @@ class CartScreen extends StatelessWidget {
                   final cartItem=cart.items[cartItemKey]!;
                   return CartItem(id: cartItemKey, title: cartItem.title, quantity: cartItem.quantity, price: cartItem.price);
                 }),
-          )
+          ),
+          buildTotalCard(cart, context),
         ],
       ),
     );
+  }
+
+  Card buildTotalCard(Cart cart, BuildContext context) {
+    return Card(
+          margin: EdgeInsets.all(12),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            //Total Amount and Order Now Button Row
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Total",
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Chip(
+                  label: Text(
+                    "\$${cart.totalAmount}",
+                    style: TextStyle(
+                        color: Theme.of(context)
+                            .primaryTextTheme
+                            .headline6!
+                            .color),
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
+                Spacer(),
+                TextButton(
+                    onPressed: () {
+                      //place the order, move to confirmation or payment screen
+                    },
+                    child: Text(
+                      "ORDER NOW",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold),
+                    ))
+              ],
+            ),
+            //Summary Cart with all items and option to remove or change their qty
+          ),
+        );
   }
 }
