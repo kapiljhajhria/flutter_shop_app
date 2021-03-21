5859a379af2e655449a02e223c7bcee00591ea0c
kapiljhajhria <jhajhria44@gmail.com>
2021-03-21 17:38:52 +0530

    showing order details on tap in orderScreen

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,11 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/cart.dart';
 import '../providers/orders.dart' as ord;
 import 'package:intl/intl.dart';
+import 'dart:math';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem orderItem;
 
   const OrderItem({Key? key, required this.orderItem});
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +24,55 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text("\$${orderItem.amount}"),
+            onTap: (){
+              setState(() {
+                _expanded = !_expanded;
+              });
+            },
+            title: Text("\$${widget.orderItem.amount}"),
             subtitle: Text(
-              DateFormat('dd-MM-yyyy hh:mm').format(orderItem.datTime),
+              DateFormat('dd-MM-yyyy hh:mm').format(widget.orderItem.datTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              height: min(widget.orderItem.products.length * 20.0 + 20, 200),
+              child: ListView.builder(
+                itemCount: widget.orderItem.products.length,
+                itemBuilder: (_, index) {
+                  final CartItem orderProduct =
+                      widget.orderItem.products[index];
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        orderProduct.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(),
+                      Text(
+                        "${orderProduct.quantity} x \$${orderProduct.price} = \$${orderProduct.price * orderProduct.quantity} ",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            )
         ],
       ),
     );
