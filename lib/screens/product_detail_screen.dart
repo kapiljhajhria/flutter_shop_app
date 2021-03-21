5ae1b9693d0dff0f1c023a6b19359f7264368509
kapiljhajhria <jhajhria44@gmail.com>
2021-03-21 13:54:55 +0530

    product detail screen now showing details about the product

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -22,6 +22,41 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                '${loadedProduct.description}',
+                textAlign: TextAlign.center,
+                softWrap: true,
+                style: TextStyle( fontSize: 18),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
