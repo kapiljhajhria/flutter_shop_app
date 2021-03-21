65a06ed8d4b40ad2266aefb89de31b89d66ef029
kapiljhajhria <jhajhria44@gmail.com>
2021-03-21 14:30:37 +0530

    order provider created

---
+++ lib/providers/orders.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/cart.dart';
+import 'package:flutter_complete_guide/providers/product.dart';
+
+class OrderItem with ChangeNotifier {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime datTime;
+
+  OrderItem(
+      {required this.id,
+      required this.amount,
+      required this.products,
+      required this.datTime});
+}
+
+class Orders with ChangeNotifier {
+  final List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+
+  addOrder(List<CartItem> cartProducts, double total) {
+    //insert so latest order it at top
+    _orders.insert(
+        0,
+        OrderItem(
+            id: DateTime.now().toString(),
+            amount: total,
+            products: cartProducts,
+            datTime: DateTime.now()));
+
+    notifyListeners();
+  }
+
+}
